70297f603719067d5aca30cf6ccebe79499d0ae8
Muskan Agnihotri <muskan21agni@gmail.com>
2026-06-05 10:41:17 +0530

    new

---
+++ prac.py
@@ -0,0 +1,9 @@
+import json 
+# create a list 4 clients
+client=[
+    {"name": "Acme Pvt Ltd", "tasks_pending": 3, "priority": "High"},
+    {"name": "Beta Corp", "tasks_pending": 1, "priority": "Low"},
+    {"name": "Gamma Inc", "tasks_pending": 5, "priority": "High"},
+    {"name": "Delta LLC", "tasks_pending": 2, "priority": "Low"}
+]
+# save to a file 
